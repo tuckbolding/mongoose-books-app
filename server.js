4921ea8b88662665577f5185a9388e7e14b61851
tuckbolding <tuckbolding@gmail.com>
2017-10-11 16:41:38 -0700

    successfully seeded the db

--- server.js
+++ server.js
@@ -5,7 +5,8 @@
 /////////////////////////////
 //  SETUP and CONFIGURATION
 /////////////////////////////
-
+//require statement to import modules
+var db = require('./models')
 //require express in our app
 var express = require('express'),
   bodyParser = require('body-parser');
@@ -25,32 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //  DATA
 ///////////////////
 
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
 
-
-var newBookUUID = 18;
 
 
 
@@ -70,12 +46,21 @@ app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
+// books index route
+
 // get all books
-app.get('/api/books', function (req, res) {
-  // send all books as JSON response
-  console.log('books index');
-  res.json(books);
-});
+// app.get('/api/books', function (req, res) {
+//   // send all books as JSON response
+//   console.log('books index');
+//   res.json(books);
+// });
+  app.get('/api/books', function (req, res) {
+    // send all books as JSON response
+    db.Book.find(function(err, books){
+      if (err) { return console.log("index error: " + err); }
+      res.json(books);
+    });
+  });
 
 // get one book
 app.get('/api/books/:id', function (req, res) {
